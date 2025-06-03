d96ca439f2ce460dac6f4cd90b7121d7f5fb6484
Enrique Velasco <evelasco0628@gmail.com>
2025-06-02 18:42:44 -0700

    Checks if authToken is still valid. If not, remove it.

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -2,5 +2,25 @@ import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem('authToken');
-  return token ? children : <Navigate to="/signin" replace />;
+
+  if (!token) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  // Checks if authToken is expired
+  try {
+    const payloadBase64 = token.split('.')[1];
+    const payloadJson = atob(payloadBase64);
+    const { exp } = JSON.parse(payloadJson);
+
+  if (exp < Math.floor(Date.now() / 1000)) {
+      localStorage.removeItem('authToken');
+      return <Navigate to="/signin" replace />;
+    }
+  } catch (e) {
+    localStorage.removeItem('authToken');
+    return <Navigate to="/signin" replace />;
+  }
+
+  return children;
 }
